e9859ff7439526f7212517f05fcd7b3d3b0179f5
Björn Petersen <git@bjoernpetersen.net>
2021-06-14 14:11:09 +0200

    Change publishing body

--- booking/main.py
+++ booking/main.py
@@ -91,10 +91,13 @@ def _is_valid(details: EventDetails) -> bool:
 
 def _publish_details(details: Iterable[EventDetails]):
     base_url = f"https://api.woog.life/lake/{configuration.lake_id}/booking"
+    body = {
+        "events": list(details)
+    }
     try:
         requests.put(
             base_url,
-            json=list(details),
+            json=body,
             headers={"Authorization": f"Bearer {configuration.api_key}"}
         )
     except Exception as e:
